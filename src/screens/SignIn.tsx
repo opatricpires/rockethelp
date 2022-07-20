881c9b3361d7227a5bcd8dbe36bc7a4929c37233
opatricpires <patric.pires@outlook.com>
2022-07-20 01:25:07 -0300

    feat(signIn): use button componetn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,22 @@
 import { Envelope, Key } from "phosphor-react-native";
 import { Heading, Icon, VStack, useTheme } from "native-base";
 
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import Logo from "../assets/logo_primary.svg";
+import { useState } from "react";
 
 export function SignIn() {
   const { colors } = useTheme();
+  const [name, setName] = useState("Patric");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        Acesse sua conta
+        Acesse sua conta {name}
       </Heading>
       <Input
         placeholder="E-mail"
@@ -18,12 +24,16 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={(text) => setEmail(text)}
       />
       <Input
+        mb={8}
         placeholder="Password"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={(text) => setPassword(text)}
       />
+      <Button title="Entrar" w="full" />
     </VStack>
   );
 }
