8ad9d33c53d8cba35ed5ad11093f6edbda561664
opatricpires <patric.pires@outlook.com>
2022-07-20 00:51:12 -0300

    feat(app): add loading fonts and statusbar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import { Box, NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
+import { Loading } from "./src/components/Loding";
 import React from "react";
 import { SignIn } from "./src/screens/SignIn";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <SignIn />;
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
