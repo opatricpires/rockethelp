1c95e6ac69e6b6e2062666cdd283c6ea6f2cf60d
opatricpires <patric.pires@outlook.com>
2022-07-20 01:10:25 -0300

    feat(signin): use initial components and logo

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { Envelope, Key } from "phosphor-react-native";
+import { Heading, Icon, VStack, useTheme } from "native-base";
+
+import { Input } from "../components/Input";
+import Logo from "../assets/logo_primary.svg";
 
 export function SignIn() {
+  const { colors } = useTheme();
   return (
-    <View>
-      <Text>Olá dev</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+      />
+      <Input
+        placeholder="Password"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+      />
+    </VStack>
   );
 }
