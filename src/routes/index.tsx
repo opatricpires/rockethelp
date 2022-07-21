4118e6536790418c3c8388eca04ebc6147acf949
opatricpires <patric.pires@outlook.com>
2022-07-21 00:54:54 -0300

    feat(routes): handle logged routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,31 @@
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { useEffect, useState } from "react";
+
 import { AppRoutes } from "./app.routes";
+import { Loading } from "../components/Loading";
 import { NavigationContainer } from "@react-navigation/native";
 import { SignIn } from "../screens/SignIn";
 
 export function Routes() {
-  const logged = true;
+  const [loading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((res) => {
+      setUser(res);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <NavigationContainer>
-      {logged ? <AppRoutes /> : <SignIn />}
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
